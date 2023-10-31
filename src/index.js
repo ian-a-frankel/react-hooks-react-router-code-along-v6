f2ca95d0280372bfe4c04e58e4454f252b69cca9
Ian Frankel <ian.frankel@gmail.com>
2023-10-31 15:26:48 -0400

    many components routed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import ErrorPage from "./pages/ErrorPage";
+import routes from "./routes.js";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Home from "./pages/Home";
 
+const router = createBrowserRouter(routes);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
